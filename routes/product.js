ee016c541d5e534c3da16c2cebfe5dde35a620c3
ngocdai99 <ngocdai99@gmail.com>
2024-11-08 23:29:19 +0700

    update product

--- routes/product.js
+++ routes/product.js
@@ -143,19 +143,18 @@ productRouter.get('/list/category/:categoryId', async function (request, respons
 productRouter.get('/detail/:productId', async function (request, response) {
     try {
 
-      
-        const {productId} = request.params
-        
+        const { productId } = request.params
+
         if (!productId) {
             return response.status(400).json({ status: false, message: "Missing productId in query params" });
         }
 
-        const productDetail = await productModel.findById({productId });
+        const productDetail = await productModel.findById( productId );
         if (!productDetail) {
             return response.status(404).json({ status: false, message: "Product not found" });
         }
 
-        response.status(200).json({ status: true, message: "Get product details successfully", productDetail});
+        response.status(200).json({ status: true, message: "Get product details successfully", productDetail });
 
     } catch (error) {
         response.status(400).json({ status: false, message: 'Http Exception 400, Bad request, get product details failed' });
